d388e4b24d87cb68dfb2ff97e0bf043248b66d08
arpan <arpan.rec@gmail.com>
2023-05-12 20:07:11 +0530

    init

---
+++ src/Main.groovy
@@ -0,0 +1,16 @@
+import jenkins.model.*
+
+def instance = Jenkins.getInstance()
+
+import hudson.security.*
+
+def realm = new HudsonPrivateSecurityRealm(false)
+instance.setSecurityRealm(realm)
+
+def strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
+
+instance.setupWizard
