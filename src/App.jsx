3e27ebaa0597d06877399ec543dff2c857e69050
arpan <arpan.rec@gmail.com>
2023-07-04 06:59:52 +0530

    vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
 import {useState} from "react";
 
-
 export default function App() {
     const [lightDarkMode, setLightDarkMode] = useState("light");
     const toggleLightDarkMode = () => {
         if (lightDarkMode === "light") {
-            document.body.style.backgroundColor = "dark";
+            document.body.style.backgroundColor = "grey";
             setLightDarkMode("dark");
         } else {
-            document.body.style.backgroundColor = "light";
+            document.body.style.backgroundColor = "white";
             setLightDarkMode("light");
         }
-    }
-    return (<>
-        <Navbar title="easyIAC1" lightDarkMode={lightDarkMode} toggleLightDarkMode={toggleLightDarkMode}/>
-        <TextForm/>
-        <About/>
-    </>);
+    };
+    return (
+        <>
+            <Navbar
+                title="easyIAC1"
+                lightDarkMode={lightDarkMode}
+                toggleLightDarkMode={toggleLightDarkMode}
+            />
+            <TextForm/>
+            <About/>
+        </>
+    );
 }
